b60642da858674ef4df0bf0ab41623374a24c1aa
ryuichi <rusami.usual@icloud.com>
2022-05-05 13:56:16 +0900

    initial commit

---
+++ bte_serial/src/main.cpp
@@ -0,0 +1,25 @@
+#include "BluetoothSerial.h"
+#include <M5Stack.h>
+
+BluetoothSerial bts;
+
+void setup() {
+  M5.begin();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0, 2);
+  M5.Lcd.println("Bluetooth TEST");
+  bts.begin("ESP32test");
+}
+
+void loop() {
+  M5.Lcd.setCursor(0, 20, 2);
+  M5.Lcd.printf("Start Bluetooth");
+  bts.println("message from the M5stack");
+  //Serial.println("loop");
+  String str = bts.readString();
+  M5.Lcd.setCursor(0, 60, 2);
+  M5.Lcd.println(str);
+
+  delay(1000);
+}
