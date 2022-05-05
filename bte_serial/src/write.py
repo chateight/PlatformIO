fcba5bd6f4e30daf23faa8e45e9890c49dabdef6
ryuichi <rusami.usual@icloud.com>
2022-05-06 08:01:34 +0900

    # to add comments

--- bte_serial/src/write.py
+++ bte_serial/src/write.py
@@ -1,9 +1,9 @@
 import serial
 
 mes = "messge from my Mac"
-# initialize bt serial
+# initialize bt serial, serial port depends on the running enviroment
 m5data = serial.Serial('/dev/tty.ESP32test',115200, timeout=3)
-# read from bt
+# read from bt and eliminate '\r\n'
 line = m5data.readline()
 print(line.decode()[:-2:])
 # write to bt
