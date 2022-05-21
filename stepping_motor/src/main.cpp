3c558a9b34cf48f36849fe57f51e87f6957ed572
ryuichi <rusami.usual@icloud.com>
2022-05-21 17:50:01 +0900

    # update rotation angle

--- stepping_motor/src/main.cpp
+++ stepping_motor/src/main.cpp
@@ -1,11 +1,16 @@
 #include <M5Stack.h>
 
+// motor/driver 28BYJ-48/ULN2003
+//
+// original source https://blog.takumus.io/entry/2016/09/13/190510
+//
+
 const int IN1 = 2;
 const int IN2 = 16;
 const int IN3 = 5;
 const int IN4 = 17;
 
-const int d_hs[8] = {
+const int d_hs[8] = {   // 1-2 drive, 4096 cycles/turn ; gear ration 64:1
   0B00001000, 
   0B00001100,
   0B00000100,
@@ -24,7 +29,7 @@ void step(int d)  // d = -1 : anticlockwise, d = 1 : clockwise
   if(i > 7) i = 0;
   if(i < 0) i = 7;
   byte b = d_hs[i];
-  digitalWrite(IN1, bitRead(b, 0));
+  digitalWrite(IN1, bitRead(b, 0));     // bit position 0 ; LSB
   digitalWrite(IN2, bitRead(b, 1));
   digitalWrite(IN3, bitRead(b, 2));
   digitalWrite(IN4, bitRead(b, 3));
@@ -40,20 +45,19 @@ void setup()
 
 void loop()
 {
-  for(int i=0; i < 256; ++i)
+  for(int i=0; i < 8*256; ++i)
   { 
     step(1);
-    delayMicroseconds(2900);
+    delayMicroseconds(2000);
   }
-  for(int i=0; i < 512; ++i)
+  for(int i=0; i < 8*512; ++i)
   {
     step(-1);
-    delayMicroseconds(2900);
+    delayMicroseconds(2000);
   }
-  for(int i=0; i < 256; ++i)
+  for(int i=0; i < 8*256; ++i)
   { 
     step(1);
-    delayMicroseconds(2900);
+    delayMicroseconds(2000);
   }
-
 }
