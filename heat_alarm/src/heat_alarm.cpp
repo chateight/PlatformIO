f361ebd31487ef49fa296bb0a8afe7db49f56e0a
ryuichi <rusami.usual@icloud.com>
2022-06-20 19:18:21 +0900

    # to change the font color

--- heat_alarm/src/heat_alarm.cpp
+++ heat_alarm/src/heat_alarm.cpp
@@ -11,7 +11,8 @@ float pressure = 0.0;
 void setup() {
     M5.begin();             // Init M5Stack
     M5.Power.begin();       // Init power  
-    M5.lcd.setTextSize(2);  // Set the text size to 2.  
+    M5.lcd.setTextSize(2);  // Set the text size to 2. 
+    M5.Lcd.setTextColor(BLACK); 
     Wire.begin();  // Wire init, adding the I2C bus.  
     qmp6988.init();
     M5.lcd.println(F("ENV Unit III test"));
@@ -28,7 +29,7 @@ void loop() {
         tmp = 0, hum = 0;
     }
     M5.lcd.fillRect(0, 20, 100, 60,
-                    BLACK);  // Fill the screen with black (to clear the
+                    WHITE);  // Fill the screen with black (to clear the
                              // screen).  
     M5.lcd.setCursor(0, 20);
     M5.Lcd.printf("Temp: %2.1f  \r\nHumi: %2.0f%%  \r\nPressure:%2.0fPa\r\n",
