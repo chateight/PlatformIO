07d2148f650a510aeeaf9b171e60ea00a5ac7e18
ryuichi <rusami.usual@icloud.com>
2022-06-20 19:20:03 +0900

    # to change comment

--- heat_alarm/src/heat_alarm.cpp
+++ heat_alarm/src/heat_alarm.cpp
@@ -12,7 +12,7 @@ void setup() {
     M5.begin();             // Init M5Stack
     M5.Power.begin();       // Init power  
     M5.lcd.setTextSize(2);  // Set the text size to 2. 
-    M5.Lcd.setTextColor(BLACK); 
+    M5.Lcd.setTextColor(BLACK);     // set font color to black
     Wire.begin();  // Wire init, adding the I2C bus.  
     qmp6988.init();
     M5.lcd.println(F("ENV Unit III test"));
@@ -20,8 +20,8 @@ void setup() {
 
 void loop() {
     pressure = qmp6988.calcPressure();
-    if (sht30.get() == 0) {  // Obtain the data of shT30. 
-        tmp = sht30.cTemp;   // Store the temperature obtained from shT30.
+    if (sht30.get() == 0) {  // Obtain the data of SHT30. 
+        tmp = sht30.cTemp;   // Store the temperature obtained from SHT30.
                              // 
         hum = sht30.humidity;  // Store the humidity obtained from the SHT30.
                                // 
@@ -29,7 +29,7 @@ void loop() {
         tmp = 0, hum = 0;
     }
     M5.lcd.fillRect(0, 20, 100, 60,
-                    WHITE);  // Fill the screen with black (to clear the
+                    WHITE);  // Fill the screen with white (to clear the
                              // screen).  
     M5.lcd.setCursor(0, 20);
     M5.Lcd.printf("Temp: %2.1f  \r\nHumi: %2.0f%%  \r\nPressure:%2.0fPa\r\n",
