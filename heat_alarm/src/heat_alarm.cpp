ae59d3fb17333c0e944f87dfeb48aadea8505c4d
ryuichi <rusami.usual@icloud.com>
2022-06-21 14:20:59 +0900

    # to set upload port/to add batt indicator

--- heat_alarm/src/heat_alarm.cpp
+++ heat_alarm/src/heat_alarm.cpp
@@ -8,12 +8,18 @@ float tmp      = 0.0;
 float hum      = 0.0;
 float pressure = 0.0;
 
+int read_batt()
+{
+  int l_battery = M5.Power.getBatteryLevel();
+  return l_battery;
+}
+
 void setup() {
     M5.begin();             // Init M5Stack
     M5.Power.begin();       // Init power  
     M5.lcd.setTextSize(2);  // Set the text size to 2. 
     M5.Lcd.setTextColor(BLACK);     // set font color to black
-    Wire.begin();  // Wire init, adding the I2C bus.  
+    Wire.begin();           // Wire init, adding the I2C bus.  
     qmp6988.init();
     M5.lcd.println(F("ENV Unit III test"));
 }
@@ -28,11 +34,14 @@ void loop() {
     } else {
         tmp = 0, hum = 0;
     }
-    M5.lcd.fillRect(0, 20, 100, 60,
-                    WHITE);  // Fill the screen with white (to clear the
-                             // screen).  
+    M5.lcd.fillScreen(WHITE);  // Fill the screen with white (to clear the screen).  
     M5.lcd.setCursor(0, 20);
     M5.Lcd.printf("Temp: %2.1f  \r\nHumi: %2.0f%%  \r\nPressure:%2.0fPa\r\n",
                   tmp, hum, pressure);
+    
+    // battery level indication
+    int bat_level = read_batt();
+    M5.Lcd.setCursor(20, 200);
+    M5.Lcd.printf("batt : %3d %%",bat_level);
     delay(2000);
 }
