c45cc656b7b40ed4c352fe8aa36e60ab113cee33
ryuichi <rusami.usual@icloud.com>
2022-06-21 17:04:08 +0900

    # to set char color according to the severity

--- heat_alarm/src/heat_alarm.cpp
+++ heat_alarm/src/heat_alarm.cpp
@@ -8,6 +8,28 @@ float tmp      = 0.0;
 float hum      = 0.0;
 float pressure = 0.0;
 
+int t_h_array[17][20] = {
+    {29, 28, 28, 27, 26, 25, 25, 24, 23, 22, 21, 21, 20, 19, 18, 18, 17, 16, 15, 15},
+    {30, 29, 28, 28, 27, 26, 25, 25, 24, 23, 22, 21, 21, 20, 19, 18, 18, 17, 16, 15},
+    {31, 30, 29, 29, 28, 27, 26, 25, 25, 24, 23, 22, 21, 21, 20, 19, 18, 17, 17, 16},
+    {32, 31, 30, 29, 29, 28, 27, 26, 25, 24, 24, 23, 22, 21, 20, 20, 19, 18, 17, 16},
+    {33, 32, 31, 30, 29, 29, 28, 27, 26, 25, 24, 24, 23, 22, 21, 20, 19, 19, 18, 17},
+    {34, 33, 32, 31, 30, 29, 29, 28, 27, 26, 25, 24, 23, 23, 22, 21, 20, 19, 18, 17},
+    {35, 34, 33, 32, 31, 30, 29, 28, 28, 27, 26, 25, 24, 23, 22, 22, 21, 20, 19, 18},
+    {35, 35, 34, 33, 32, 31, 30, 29, 28, 27, 27, 26, 25, 24, 23, 22, 21, 20, 19, 19},
+    {36, 35, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 25, 24, 23, 22, 21, 20, 19},
+    {37, 36, 35, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 22, 21, 20},
+    {38, 37, 36, 35, 34, 33, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20},
+    {39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 29, 28, 27, 26, 25, 24, 23, 22, 21},
+    {40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21},
+    {41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22},
+    {42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23},
+    {43, 42, 41, 40, 39, 38, 37, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23},
+    {44, 43, 42, 41, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24}
+};
+
+int t_color[4] = {GREEN, YELLOW, ORANGE, RED};
+
 int read_batt()
 {
   int l_battery = M5.Power.getBatteryLevel();
@@ -38,6 +60,40 @@ void loop() {
     M5.lcd.setCursor(0, 20);
     M5.Lcd.printf("Temp: %2.1f  \r\nHumi: %2.0f%%  \r\nPressure:%2.0fPa\r\n",
                   tmp, hum, pressure);
+    
+    // heat alarm caluc
+    int temp_int = (int)(tmp+0.5);              // four and five in
+    int hum_five = (int)(hum + 2.5)/5*5;        // round to five
+
+    if (temp_int > 40)
+    {
+        temp_int = 40;
+    }
+    if (hum_five < 20)
+    {
+        hum_five = 20;
+    }
+    int array_hum = (hum_five - 20)/5;          // convert to the element number of the temp/humi array
+    int array_temp = 40 - temp_int;
+    int heat_alarm_value = t_h_array[array_hum][array_temp];    // access to the temp/humi array
+    // print out the result
+    M5.lcd.setTextSize(4);
+    if (heat_alarm_value <= 25 ){
+        M5.Lcd.setTextColor(GREEN);
+    }
+    if (heat_alarm_value >= 25 && heat_alarm_value <= 27){
+        M5.Lcd.setTextColor(YELLOW);
+    }
+    if (heat_alarm_value >= 28 && heat_alarm_value <= 30 ){
+        M5.Lcd.setTextColor(ORANGE);
+    }
+    if (heat_alarm_value > 30 ){
+        M5.Lcd.setTextColor(RED);
+    }
+    M5.lcd.setCursor(20, 120);
+    M5.Lcd.printf("alarm_lv:%d", heat_alarm_value);
+    M5.Lcd.setTextColor(BLACK);
+    M5.lcd.setTextSize(2);
     
     // battery level indication
     int bat_level = read_batt();
